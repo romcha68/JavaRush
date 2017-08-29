c9ddb4d8e6e333f1c93f14f06ce4a7c8a4410f64
AstureS <a.romancha@hotmail.com>
2017-08-29 15:11:30 +0300

    Решение задачи на выбор 5 max чисел в сиске

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0826;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        for(int i=0;i<array.length;i++){
+            for(int j=array.length-2;j>=i;j--){
+                if(array[j]<array[j+1]){
+                    int temp=array[j+1];
+                    array[j+1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+    }
+}

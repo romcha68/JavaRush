f84a4be88271fd868f293b26e7a1a05f9e7fea83
AstureS <a.romancha@hotmail.com>
2017-09-07 11:46:59 +0300

    Определить номер строки кода в стеке

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task09.task0903;
+
+/* 
+Кто меня вызывал?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static int method1() {
+        method2();
+        return  /*напишите тут ваш код*/Thread.currentThread().getStackTrace()[2].getLineNumber();
+
+    }
+
+    public static int method2() {
+        method3();
+        return  /*напишите тут ваш код*/ Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        return  /*напишите тут ваш код*/Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        return  /*напишите тут ваш код*/ Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static int method5() {
+        return  /*напишите тут ваш код*/ Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+}

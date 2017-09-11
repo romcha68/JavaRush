07ec8fd16dd9e574b357b4044b4581665b96367a
AstureS <a.romancha@hotmail.com>
2017-09-12 02:42:47 +0300

    Перехват выборочных исключений

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0915/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task09.task0915;
+
+import java.io.CharConversionException;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+
+/* 
+Перехват выборочных исключений
+*/
+
+public class Solution {
+    public static StatelessBean BEAN = new StatelessBean();
+
+    public static void main(String[] args) {
+        try{processExceptions();
+        }
+        catch (Exception ex){
+            BEAN.log(ex);
+        }
+    }
+
+    public static void processExceptions() throws FileSystemException {
+        try {
+            BEAN.methodThrowExceptions();
+        }
+        catch (FileSystemException ex){
+            BEAN.log(ex);
+            throw new FileSystemException(ex.getFile());
+        }
+        catch (IOException ex){
+            BEAN.log(ex);
+        }
+    }
+
+
+    public static class StatelessBean {
+        public void log(Exception exception) {
+            System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
+        }
+
+        public void methodThrowExceptions() throws CharConversionException, FileSystemException, IOException {
+            int i = (int) (Math.random() * 3);
+            if (i == 0)
+                throw new CharConversionException();
+            if (i == 1)
+                throw new FileSystemException("");
+            if (i == 2)
+                throw new IOException();
+        }
+    }
+}

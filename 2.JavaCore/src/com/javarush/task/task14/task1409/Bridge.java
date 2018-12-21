ebcd92cd1cb58f7ed7e692c33b4067bb425c5b9d
romcha68 <romcha68@ukr.net>
2018-12-21 15:53:50 +0200

    Мосты

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/Bridge.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1409;
+
+/**
+ * Created by User on 20.12.2018.
+ */
+public interface Bridge {
+    int getCarsCount();
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/SuspensionBridge.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1409;
+
+/**
+ * Created by User on 20.12.2018.
+ */
+public class SuspensionBridge implements Bridge {
+    public int getCarsCount(){
+        return 505;
+    }
+}

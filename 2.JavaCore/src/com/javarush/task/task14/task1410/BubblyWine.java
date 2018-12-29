1228499f1ad8ae3ae9bb9edd904125c22baec1b3
romcha68 <romcha68@ukr.net>
2018-12-29 13:28:51 +0200

    Дегустация вин

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by User on 26.12.2018.
+ */
+public class BubblyWine extends Wine {
+    public String getHolidayName(){
+        return "Новый Год";
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Wine.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by User on 26.12.2018.
+ */
+public class Wine extends Drink {
+    public String getHolidayName(){
+        return "День Рождения";
+    }
+}

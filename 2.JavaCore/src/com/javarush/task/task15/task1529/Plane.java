573ce82fcbdd6b834e8c9f7832bce1ce73ef9d65
romcha68 <romcha68@ukr.net>
2019-10-16 16:20:53 +0300

    Осваивание статического блока.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by User on 16.10.2019.
+ */
+public class Plane implements CanFly{
+    public void fly(){
+    }
+    public Plane(int a) {
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Осваивание статического блока
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    static {
+        //add your code here - добавьте код тут
+        reset();
+    }
+
+    public static CanFly result;
+
+    public static void reset() {
+        //add your code here - добавьте код тут
+        Scanner scanner = new Scanner(System.in);
+
+            String aircraft = scanner.nextLine();
+            try{if(aircraft.equals("helicopter")){
+                result = new Helicopter();
+            }
+            if (aircraft.equals("plane")){
+                result = new Plane(Integer.parseInt(scanner.nextLine()));
+            }
+            scanner.close();}
+            catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+

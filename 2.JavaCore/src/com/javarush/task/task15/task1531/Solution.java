5d2d1a65d83ecaf118e21fb81f802e94354c69cc
romcha68 <romcha68@ukr.net>
2019-10-18 15:03:17 +0300

    Факториал.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task15.task1531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/* 
+Факториал
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        //add your code here
+        String wholeResult = null;
+        if (n < 0) {
+            return  "0";
+        }
+        if(n == 0){
+            return "1";
+        }
+        else if (n <= 150) {
+            BigInteger result = BigInteger.ONE;
+            for (int i = 1; i <= n; i++) {
+
+                result = result.multiply(BigInteger.valueOf(i));
+                wholeResult = String.valueOf(result);
+            }
+        }
+            return wholeResult;
+        }
+
+}
